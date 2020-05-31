c5ec202ba24382685735db1bfe244044813d21a8
hanhanlvlv <1248612588@qq.com>
2020-05-31 13:29:43 +0800

    用链表写一个栈

--- src/main/java/com/atguigu/dataStructures/StackArrDemo.java
+++ src/main/java/com/atguigu/dataStructures/StackArrDemo.java
@@ -1,45 +1,142 @@
 package com.atguigu.dataStructures;
 
+import javafx.beans.binding.BooleanExpression;
+
 import java.util.Scanner;
 
 public class StackArrDemo {
 
     public static void main(String[] args) {
 
-        StackArr stackArr = new StackArr(4);
+//        StackArr stackArr = new StackArr(4);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String key = "";
+//        boolean loop = true;
+//        while (loop){
+//
+//            key = scanner.next();
+//            switch (key){
+//                case "push":
+//                    System.out.println("请输入一个数");
+//                    int value = scanner.nextInt();
+//                    stackArr.push(value);
+//                    break;
+//                case "pop":
+//                    try {
+//                        System.out.println("取出的值为" + stackArr.pop());
+//                    }catch (Exception e){
+//                        System.out.println(e.getMessage());
+//                    }
+//                    break;
+//                case "show":
+//                    stackArr.showStack();
+//                    break;
+//                case "exit":
+//                    scanner.close();
+//                    loop = false;
+//                    break;
+//            }
+//
+//        }
 
-        Scanner scanner = new Scanner(System.in);
-        String key = "";
-        boolean loop = true;
-        while (loop){
+        StackLinked stackLinked = new StackLinked();
 
-            key = scanner.next();
-            switch (key){
-                case "push":
-                    System.out.println("请输入一个数");
-                    int value = scanner.nextInt();
-                    stackArr.push(value);
-                    break;
-                case "pop":
-                    try {
-                        System.out.println("取出的值为" + stackArr.pop());
-                    }catch (Exception e){
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case "show":
-                    stackArr.showStack();
-                    break;
-                case "exit":
-                    scanner.close();
-                    loop = false;
-                    break;
-            }
+        stackLinked.add(new LinkedStack(1,"宋江"));
+        stackLinked.add(new LinkedStack(2,"卢俊义"));
+        stackLinked.add(new LinkedStack(3,"晁盖"));
+        stackLinked.add(new LinkedStack(4,"吴用"));
 
+        System.out.println("原始数据：");
+        stackLinked.showLisi();
+
+        LinkedStack linkedStack1 = stackLinked.get();
+        System.out.println("获取第一条数据之后，剩余的数据：" + linkedStack1);
+        stackLinked.showLisi();
+
+        LinkedStack linkedStack2 = stackLinked.get();
+        System.out.println("再次获取数据之后，剩余的数据：" + linkedStack2);
+        stackLinked.showLisi();
+
+        LinkedStack linkedStack3 = stackLinked.get();
+        System.out.println("再次获取数据之后，剩余的数据：" + linkedStack3);
+        stackLinked.showLisi();
+
+        LinkedStack linkedStack4 = stackLinked.get();
+        System.out.println("再次获取数据之后，剩余的数据：" + linkedStack4);
+        stackLinked.showLisi();
+
+    }
+
+}
+
+
+class StackLinked{
+
+    private LinkedStack head = new LinkedStack(0,"");
+
+    public void add(LinkedStack linkedStack){
+
+        if (head.next == null){
+            head.next = linkedStack;
+        }else {
+            linkedStack.next = head.next;
+            head.next = linkedStack;
         }
 
     }
 
+
+    public LinkedStack get(){
+
+        if (head.next == null){
+            System.out.println("链表中没有数据");
+            return null;
+        }
+        LinkedStack next = head.next;
+        head.next = head.next.next;
+
+        return next;
+    }
+
+
+    public void showLisi(){
+
+        if (head.next == null){
+            System.out.println("链表维欧空");
+            return;
+        }
+
+        LinkedStack temp = head.next;
+        while (temp != null){
+            System.out.println("链表中的数据：" + temp);
+            temp = temp.next;
+        }
+
+    }
+
+
+}
+
+
+class LinkedStack{
+
+    public int no;
+    public String name;
+    public LinkedStack next;
+
+    @Override
+    public String toString() {
+        return "LinkedStack{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public LinkedStack(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
 }
 
 
